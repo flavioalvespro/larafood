73f6cc1191ea1e26b7e6c969721c58cfe0590579
Flavio Alves <flavioalvespro@gmail.com>
2020-05-02 16:06:06 -0300

    work with plans

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,52 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+    <h1>Planos <a href="{{ route('plans.create') }}" class="btn btn-dark">Adicionar</a></h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="header">
+           <form action="{{ route('plans.search') }}" method="POST" class="form form-inline">
+               @csrf
+                <input type="text" name="filter" id="filter" placeholder="Nome" class="form-control" value={{ $filters['filter'] ?? '' }}>
+                <button type="submit" class="btn btn-dark">Filtrar</button>
+           </form>
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th width="50">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                            <td>
+                                {{ $plan->name }}
+                            </td>
+                            <td>
+                                {{ number_format($plan->price, 2, ',', '.') }}
+                            </td>
+                            <td>
+                                <a href="{{ route('plans.show', $plan->url) }}" class="btn btn-warning">VER</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            @if(isset($filters))
+                {!! $plans->appends($filters)->links() !!}
+            @else
+                {!! $plans->links() !!}
+            @endif
+        </div>
+    </div>
+@stop
