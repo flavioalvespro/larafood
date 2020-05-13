892c1cd40303a66b66dc7a9e0548f3f7d77cf240
Flavio Alves <flavioalvespro@gmail.com>
2020-05-13 09:30:14 -0300

    start working with profiles

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->namespace('Admin')->group(function(){
     
     /**
+     * Routes Profiles
+     */
+
+    
+    Route::resource('profiles', 'ACL\ProfileController');
+    
+    /**
      * routes details plan
      */
     Route::delete('plans/details/{url}/{idDetail}', 'DetailPlanController@delete')->name('details.plan.delete');
