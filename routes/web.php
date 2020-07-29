0e9959a45feb3759295d6236c7acb2d927cc6f57
Flavio Alves <flavioalvespro@gmail.com>
2020-07-29 16:55:49 -0300

    vinculando permissão ao perfil

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
     /**
      * routes permission x profiles
      */
+    Route::post('profiles/{id}/permissions', 'ACL\PermissionProfileController@attachPermissionsProfile')->name('profiles.permissions.attach');
+    Route::get('profiles/{id}/permissions/create', 'ACL\PermissionProfileController@permissionsAvailable')->name('profiles.permissions.available');
     Route::get('profiles/{id}/permissions', 'ACL\PermissionProfileController@permissions')->name('profiles.permissions');
     
     /**
