cd20a03cabdb4adf969fa9b156eb5afb33f0735f
Flavio Alves <flavioalvespro@gmail.com>
2020-09-14 16:14:08 -0300

    adicao de home de site no projeto

--- routes/web.php
+++ routes/web.php
@@ -78,9 +78,7 @@ Route::prefix('admin')
     Route::get('/', 'PlanController@index')->name('admin.index');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Site\SiteController@index')->name('site.home');
 
 /**
  * Auth routes
