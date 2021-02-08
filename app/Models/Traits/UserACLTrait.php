4f721012e7e4d9315e0c006f9e0b85465d55e128
Flavio Alves <flavioalvespro@gmail.com>
2021-02-08 09:27:42 -0300

    add role for user with event

--- app/Models/Traits/UserACLTrait.php
+++ app/Models/Traits/UserACLTrait.php
@@ -2,17 +2,50 @@
 
 namespace App\Models\Traits;
 
+use App\Models\Tenant;
+
 trait UserACLTrait
 {
     public function permissions()
     {
-        $tenant = $this->tenant()->first();
+        $permissionsPlan = $this->permissionsPlan();
+        $permissionsRole = $this->permissionsRole();
+
+        $permissions = [];
+
+        foreach ($permissionsRole as $permission) {
+            if (in_array($permission, $permissionsPlan)) {
+                $permissions[] = $permission;
+            }
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsPlan()
+    {
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
         $plan = $tenant->plan;
-        
+
         $permissions = [];
 
         foreach ($plan->profiles as $profile) {
             foreach ($profile->permissions as $permission) {
+                $permissions[] = $permission->name;
+            }
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsRole()
+    {
+        $roles = $this->roles()->with('permissions')->get();
+
+        $permissions = [];
+
+        foreach ($roles as $role) {
+            foreach ($role->permissions as $permission) {
                 $permissions[] = $permission->name;
             }
         }
