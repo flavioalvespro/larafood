46aca922a82591ad2a0ff9edaed0f9a0b356e086
Flavio Alves <flavioalvespro@gmail.com>
2021-02-23 09:27:06 -0300

    working with clients register in API

---
+++ app/Services/ClientService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\ClientRepositoryInterface;
+
+class ClientService
+{
+    private $clientRepository;
+
+    public function __construct(
+        ClientRepositoryInterface $clientRepository
+    )
+    {
+        $this->clientRepository = $clientRepository;
+    }
+
+    public function createNewClient(array $data)
+    {
+        return $this->clientRepository->createNewClient($data);   
+    }
+}
